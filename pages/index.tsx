5e300f1e4d6883898966dad6a1752c6fac3d5fa4
Joca Sousa <jocasousac@gmail.com>
2022-09-30 15:21:33 -0300

    configure .env variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
+const boxStyles = { padding: '12px', border: '1px solid #eaeaea', borderRadius: '10px' };
+
+
 const Home: NextPage = () => {
   return (
      <div>
       <h1 className={styles.title}>Sentry Config</h1>
+
+      <button type="button" style={{
+                    ...boxStyles,
+                    backgroundColor: '#c73852',
+                    borderRadius: '12px',
+                    border: 'none'
+                }} onClick={() => {
+                    throw new Error("Sentry Frontend Error");
+                }}>
+                    Throw error
+                </button>
+
      </div>
   )
 }
